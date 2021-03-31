904f899e5b515582ffcb2ff1c4399d34c91dd5d4
Shadow <qpwo5470@gmail.com>
2021-03-31 16:55:36 +0900

    initial commit

--- post.php
+++ post.php
@@ -1,11 +1,12 @@
 <?php
 $device = $_POST['device'];
 $uid = $_POST['uid'];
+$time = time();
 
 $conn = mysqli_connect(
     'localhost',
     'daelim',
     'daelim',
     'visitors');
-$sql = "INSERT INTO time(uid, zone0) VALUES ('$uid')";
+$sql = "INSERT INTO time(uid, zone0) VALUES ('$uid', '$time')";
 $result = mysqli_query($conn, $sql);
