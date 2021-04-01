e9c099a9e260b28ae827e3b91efa159dc11f7b63
Shadow <qpwo5470@gmail.com>
2021-04-01 20:53:59 +0900

    initial commit

--- monitor.php
+++ monitor.php
@@ -9,10 +9,11 @@ $sql = "SELECT * FROM checklist";
 
 $data = mysqli_query($conn, $sql);
 while ($datum = mysqli_fetch_array($data)) {
+    echo "DEVICE "
     echo (int)$datum['device'];
-    echo ',';
+    echo '    ';
     $connection = (int)time()-(int)$datum['time']<30;
     echo $connection? "CONNECTED":"DISCONNECTED";
 }
-echo '<br>';
+echo "<br>";
 ?>
