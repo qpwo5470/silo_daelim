48b8671e55b09d70a70299458b40ddfd2626f0c1
Shadow <qpwo5470@gmail.com>
2021-04-01 20:27:33 +0900

    initial commit

--- monitor.php
+++ monitor.php
@@ -5,15 +5,13 @@ $conn = mysqli_connect(
     'daelim',
     'check');
 
-for($i = 0; $i <10; $i++) {
-    $sql = "SELECT * FROM checklist";
+$sql = "SELECT * FROM checklist";
 
-    $data = mysqli_query($conn, $sql);
-    while ($datum = mysqli_fetch_array($data)) {
-        echo (int)$datum['device'];
-        echo ',';
-        echo (int)$datum['time'];
-    }
-    echo '\n';
+$data = mysqli_query($conn, $sql);
+while ($datum = mysqli_fetch_array($data)) {
+    echo (int)$datum['device'];
+    echo ',';
+    echo (int)$datum['time'];
 }
+echo '\n';
 ?>
