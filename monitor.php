81e0f366aba8cfab61dfb43dd6d6a1ddc00d31c8
Shadow <qpwo5470@gmail.com>
2021-04-01 18:47:36 +0900

    initial commit

---
+++ monitor.php
@@ -0,0 +1,13 @@
+<?php
+$time = time();
+
+$conn = mysqli_connect(
+    'localhost',
+    'daelim',
+    'daelim',
+    'check');
+
+$sql = "SELECT * FROM checklist";
+$data = mysqli_query($conn, $sql);
+mysqli_close($conn);
+print($data);
