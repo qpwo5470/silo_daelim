f1cea523dd371fac7f6cb6aa7322fb59254e93c9
Shadow <qpwo5470@gmail.com>
2021-04-06 19:17:53 +0900

    initial commit

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+$device = $_POST['device'];
+$uid = $_POST['uid'];
+
+$conn = mysqli_connect(
+    'localhost',
+    'daelim',
+    'daelim',
+    'check');
+
+$sql = "SELECT '$uid' FROM time";
+
+$data = mysqli_query($conn, $sql);
+while ($datum = mysqli_fetch_array($data)) {
+    echo $datum['uid'];
+    echo '/';
+    echo $datum['zone1'];
+    echo '/';
+    echo $datum['zone2'];
+    echo '/';
+    echo $datum['zone3'];
+    echo '/';
+    echo $datum['zone4'];
+    echo '/';
+    echo $datum['zone5'];
+    echo '/';
+    echo $datum['zone6'];
+    echo "\n";
+}
+?>
