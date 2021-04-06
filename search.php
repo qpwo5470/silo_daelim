1e446106413691937ef7c63ade98bc2ccb26e8bc
Shadow <qpwo5470@gmail.com>
2021-04-06 19:35:15 +0900

    initial commit

--- search.php
+++ search.php
@@ -1,31 +1,30 @@
 <?php
 $uid = $_POST['uid'];
-echo $uid;
 
-//$conn = mysqli_connect(
-//    'localhost',
-//    'daelim',
-//    'daelim',
-//    'visitors');
-//
-//$sql = "SELECT * FROM time";
-//
-//$data = mysqli_query($conn, $sql);
-//while ($datum = mysqli_fetch_array($data)) {
-//    echo $uid;
-//    echo $datum['uid'];
-//    echo '/';
-//    echo $datum['zone1'];
-//    echo '/';
-//    echo $datum['zone2'];
-//    echo '/';
-//    echo $datum['zone3'];
-//    echo '/';
-//    echo $datum['zone4'];
-//    echo '/';
-//    echo $datum['zone5'];
-//    echo '/';
-//    echo $datum['zone6'];
-//    echo "\n";
-//}
+$conn = mysqli_connect(
+    'localhost',
+    'daelim',
+    'daelim',
+    'visitors');
+
+$sql = "SELECT * FROM time where uid='$uid'";
+
+$data = mysqli_query($conn, $sql);
+while ($datum = mysqli_fetch_array($data)) {
+    echo $uid;
+    echo $datum['uid'];
+    echo '/';
+    echo $datum['zone1'];
+    echo '/';
+    echo $datum['zone2'];
+    echo '/';
+    echo $datum['zone3'];
+    echo '/';
+    echo $datum['zone4'];
+    echo '/';
+    echo $datum['zone5'];
+    echo '/';
+    echo $datum['zone6'];
+    echo "\n";
+}
 ?>
