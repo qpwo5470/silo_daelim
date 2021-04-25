4822a02aab5231b30c5542e0afbc33896461a3d3
Shadow <qpwo5470@gmail.com>
2021-04-26 03:32:34 +0900

    lucky

--- monitor.php
+++ monitor.php
@@ -9,12 +9,11 @@ $sql = "SELECT * FROM checklist";
 
 $data = mysqli_query($conn, $sql);
 
-$zones = ["DREAM STATION", "STUDIO 401", "STUDIO 402", "STUDIO 403", "COMMUNITY CENTER", "DREAM LOUNGE"];
+$zones = ["DREAM STATION\t", "STUDIO 401\t", "STUDIO 402\t", "STUDIO 403\t", "COMMUNITY CENTER", "DREAM LOUNGE\t"];
 
 while ($datum = mysqli_fetch_array($data)) {
-    echo "DEVICE ";
-    echo $zones[(int)$datum['device']];
-    echo "\t";
+    echo $zones[(int)$datum['device']-1];
+    echo "\t\t";
     $connection = (int)time()-(int)$datum['time']<30;
     echo $connection? "CONNECTED":"DISCONNECTED";
     echo "<br>";
