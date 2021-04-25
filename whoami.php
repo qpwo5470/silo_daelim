8bc2e3669c1f7d4cac5ec5ff95c74a1559e58c6d
Shadow <qpwo5470@gmail.com>
2021-04-26 04:59:38 +0900

    lucky

--- whoami.php
+++ whoami.php
@@ -1,2 +1,21 @@
 <?php
 echo exec('whoami');
+$time = time();
+
+date_default_timezone_set("Asia/Seoul");
+$date = date('Y-m-d', $time);
+
+
+$conn = mysqli_connect(
+    'localhost',
+    'daelim',
+    'daelim',
+    'visitors');
+
+
+$randTime = '05:01:00';
+$sql = "UPDATE luckytime SET time = '$randTime' WHERE date = '$date'";
+mysqli_query($conn, $sql);
+
+$sql = "TRUNCATE TABLE luckytoday";
+mysqli_query($conn, $sql);
