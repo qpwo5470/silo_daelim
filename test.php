f90bfb7208e6ef0394a1e9f54eeacbb2b0fbfa2a
Shadow <qpwo5470@gmail.com>
2021-04-27 16:53:53 +0900

    range

--- test.php
+++ test.php
@@ -15,17 +15,17 @@ $sql = "SELECT * FROM luckyrange WHERE no = 0";
 $array = mysqli_query($conn, $sql);
 echo $array;
 echo "<br>";
-$range = mysqli_fetch_array($array)[0];
-echo $range['start'];
-echo "<br>";
-echo $range['end'];
-echo "<br>";
-$startTime = $range['start'] . explode(':');
-$endTime = $range['end'] . explode(':');
-
-$randDate = new DateTime();
-$randDate->setTime(mt_rand($startTime[0], $endTime[0]), mt_rand($startTime[1], $endTime[1]), mt_rand($startTime[2], $endTime[2]));
-$randTime = $randDate->format('H:i:s');
-
-echo "<br>";
-echo $randTime;
+//$range = mysqli_fetch_array($array)[0];
+//echo $range['start'];
+//echo "<br>";
+//echo $range['end'];
+//echo "<br>";
+//$startTime = $range['start'] . explode(':');
+//$endTime = $range['end'] . explode(':');
+//
+//$randDate = new DateTime();
+//$randDate->setTime(mt_rand($startTime[0], $endTime[0]), mt_rand($startTime[1], $endTime[1]), mt_rand($startTime[2], $endTime[2]));
+//$randTime = $randDate->format('H:i:s');
+//
+//echo "<br>";
+//echo $randTime;
