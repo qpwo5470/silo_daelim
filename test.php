a9cff962bd5ac68a4ffea9a58f7b28b5956e2e26
Shadow <qpwo5470@gmail.com>
2021-04-27 17:11:33 +0900

    range

--- test.php
+++ test.php
@@ -20,8 +20,8 @@ while ($range = mysqli_fetch_array($data)) {
     echo $range['end'];
     echo "<br>";
 
-    $startTime = "".$range['start'];
-    $endTime = "".$range['end'];
+    $startTime = "".$range['start']->explode(':');
+    $endTime = "".$range['end']->explode(':');
 
     echo $startTime;
     echo "<br>";
