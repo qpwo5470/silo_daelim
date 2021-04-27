a246e7dc4b3cbb3a21b6078f20ef550158be81a7
Shadow <qpwo5470@gmail.com>
2021-04-27 17:16:31 +0900

    range

--- test.php
+++ test.php
@@ -20,17 +20,19 @@ while ($range = mysqli_fetch_array($data)) {
     echo $range['end'];
     echo "<br>";
 
-    $startTime = explode(":", $range['start']);
-    $endTime = explode(":", $range['end']);
+    $startTime = $range['start'];
+    $endTime = $range['end'];
 
     echo $startTime;
     echo "<br>";
     echo $endTime;
     echo "<br>";
 
-    $randDate = new DateTime();
-    $randDate->setTime(mt_rand((int)$startTime[0], (int)$endTime[0]), mt_rand((int)$startTime[1], (int)$endTime[1]), mt_rand((int)$startTime[2], (int)$endTime[2]));
-    $randTime = $randDate->format('H:i:s');
+    do {
+        $randDate = new DateTime();
+        $randDate->setTime(mt_rand((int)explode(":", $startTime)[0], (int)explode(":", $endTime)[0]), mt_rand((int)$startTime[1], (int)$endTime[1]), mt_rand((int)$startTime[2], (int)$endTime[2]));
+        $randTime = $randDate->format('H:i:s');
+    } while(strtotime($randTime)<strtotime($startTime) || strtotime($endTime)<strtotime($randTime));
 
     echo "<br>";
     echo $randTime;
